b55546e592f5e26dd766a42109830c7c5803dab3
eduardotec05 <eduardo.dominguez7707@alumnos.udg.mx>
2025-02-04 20:43:18 -0600

    Add the code in the file js, and describe the types of datas in js

--- clase-4-02-2025/script.js
+++ clase-4-02-2025/script.js
@@ -2,14 +2,55 @@
     TIPOS DE DATOS EN JAVASCRIPT
 */
 // Tipo: Number (Número)
+var edad = 25;
+var precio = 10.5;
+var puntos = 0;
 // Tipo: String (Cadena de texto)
+var nombre = "Juan";
+var saludo = "Hola";
 // Tipo: Boolean (Booleano)
+var esVerdadero = true;
+var esFalso = false;
 // Tipo: Undefined (Indefinido)
+var indefinido;
+
 // Tipo: Null (Nulo)
+var nulo = null;
 // Tipo: Symbol (Símbolo)
+var simbolo = Symbol("simbolo");
 
-// Tipo: Object (Objeto)
-// Tipo: Array (Matriz)
+//Tipo NaN (No es un número)
+var noEsNumero = "Hola" * 10;
 
-// Tipo: Function (Función)
-// Tipo: Date (Fecha)
+console.log(edad);
+console.log(precio);
+console.log(puntos);
+console.log(nombre);
+console.log(saludo);
+console.log(esVerdadero);
+console.log(esFalso);
+console.log(indefinido);
+console.log(nulo);
+console.log(simbolo);
+console.log(noEsNumero);
+
+/*
+console log se usa para mostrar valores en la consola del navegador.
+Es útil para ver los valores de variables y para depurar el código.
+ */
+
+/* El typeof devuelve el tipo de dato de una variable.
+Es útil para saber el tipo de dato de una variable.*/
+
+console.log("Tipos de datos en JavaScript:");
+console.log(typeof edad);
+console.log(typeof precio);
+console.log(typeof puntos);
+console.log(typeof nombre);
+console.log(typeof saludo);
+console.log(typeof esVerdadero);
+console.log(typeof esFalso);
+console.log(typeof indefinido);
+console.log(typeof nulo);
+console.log(typeof simbolo);
+console.log(typeof noEsNumero);
