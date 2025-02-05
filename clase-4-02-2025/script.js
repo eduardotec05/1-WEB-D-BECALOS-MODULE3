a450409e7a93e44af7faa07a511b20970ed40db6
eduardotec05 <eduardo.dominguez7707@alumnos.udg.mx>
2025-02-04 20:08:45 -0600

    Create the files

---
+++ clase-4-02-2025/script.js
@@ -0,0 +1,15 @@
+/*
+    TIPOS DE DATOS EN JAVASCRIPT
+*/
+// Tipo: Number (Número)
+// Tipo: String (Cadena de texto)
+// Tipo: Boolean (Booleano)
+// Tipo: Undefined (Indefinido)
+// Tipo: Null (Nulo)
+// Tipo: Symbol (Símbolo)
+
+// Tipo: Object (Objeto)
+// Tipo: Array (Matriz)
+
+// Tipo: Function (Función)
+// Tipo: Date (Fecha)
